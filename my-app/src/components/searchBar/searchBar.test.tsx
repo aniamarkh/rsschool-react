3d90c100a11b75f9851f22c06132ecf7cf85f5cc
aniamarkh <aniamarkh@gmail.com>
2023-03-20 00:06:00 +0300

    fix: remove unnecessary imports

--- my-app/src/components/searchBar/searchBar.test.tsx
+++ my-app/src/components/searchBar/searchBar.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { describe, test, expect } from 'vitest';
 import { render } from '@testing-library/react';
 import SearchBar from './searchBar';
-import userEvent from '@testing-library/user-event';
 
 describe('SearchBar', () => {
   test('renders without errors', () => {
