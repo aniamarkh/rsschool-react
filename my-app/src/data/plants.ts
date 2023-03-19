359d18391d1f59697bffc2733a9a1f7be627cc36
aniamarkh <aniamarkh@gmail.com>
2023-03-19 15:08:08 +0300

    feat: add data for cards

---
+++ my-app/src/data/plants.ts
@@ -0,0 +1,122 @@
+export const plantsData = [
+  {
+    imgSrc: '',
+    imgAlt: 'Fiddle Leaf Fig',
+    title: 'Fiddle Leaf Fig',
+    petFriendly: false,
+    inStock: 7,
+    price: 25.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Snake Plant',
+    title: 'Snake Plant',
+    petFriendly: true,
+    inStock: 4,
+    price: 12.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Rubber Plant',
+    title: 'Rubber Plant',
+    petFriendly: false,
+    inStock: 9,
+    price: 18.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'ZZ Plant',
+    title: 'ZZ Plant',
+    petFriendly: true,
+    inStock: 5,
+    price: 19.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Monstera Deliciosa',
+    title: 'Monstera Deliciosa',
+    petFriendly: false,
+    inStock: 6,
+    price: 21.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Pothos',
+    title: 'Pothos',
+    petFriendly: true,
+    inStock: 10,
+    price: 8.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Boston Fern',
+    title: 'Boston Fern',
+    petFriendly: true,
+    inStock: 2,
+    price: 15.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Peace Lily',
+    title: 'Peace Lily',
+    petFriendly: false,
+    inStock: 3,
+    price: 14.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Spider Plant',
+    title: 'Spider Plant',
+    petFriendly: true,
+    inStock: 8,
+    price: 11.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Bird of Paradise',
+    title: 'Bird of Paradise',
+    petFriendly: false,
+    inStock: 1,
+    price: 29.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Philodendron',
+    title: 'Philodendron',
+    petFriendly: false,
+    inStock: 7,
+    price: 17.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Chinese Evergreen',
+    title: 'Chinese Evergreen',
+    petFriendly: false,
+    inStock: 5,
+    price: 12.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Aloe Vera',
+    title: 'Aloe Vera',
+    petFriendly: true,
+    inStock: 4,
+    price: 13.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'English Ivy',
+    title: 'English Ivy',
+    petFriendly: false,
+    inStock: 3,
+    price: 9.99
+  },
+  {
+    imgSrc: '',
+    imgAlt: 'Croton',
+    title: 'Croton',
+    petFriendly: false,
+    inStock: 6,
+    price: 23.99
+  },
+]
