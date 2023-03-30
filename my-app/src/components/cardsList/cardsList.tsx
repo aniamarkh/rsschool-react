4f1a10994d18bc8ace82197a44b92bc04a0b9681
aniamarkh <aniamarkh@gmail.com>
2023-03-30 16:18:44 +0300

    feat: rewrite CardsList component

--- my-app/src/components/cardsList/cardsList.tsx
+++ my-app/src/components/cardsList/cardsList.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import PlantCard from '../card/card';
 import { PlantData } from '../../types/types';
 
-export default class CardsList extends React.Component<{ data: PlantData[] }> {
-  render() {
-    const { data } = this.props;
+interface CardsListProps {
+  data: PlantData[];
+}
 
-    return (
-      <ul className="card-list">
-        {data.map((item: PlantData) => (
-          <li key={item.id} className="card-list__item">
-            <PlantCard {...item} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
+export default function CardsList(props: CardsListProps) {
+  const { data } = props;
+
+  return (
+    <ul className="card-list">
+      {data.map((item: PlantData) => (
+        <li key={item.id} className="card-list__item">
+          <PlantCard {...item} />
+        </li>
+      ))}
+    </ul>
+  );
 }
