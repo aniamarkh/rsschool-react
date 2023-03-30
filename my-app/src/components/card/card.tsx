35389e0f51a4789be1970618a00226489d0ecf5c
aniamarkh <aniamarkh@gmail.com>
2023-03-30 16:09:54 +0300

    feat: rewrite card component

--- my-app/src/components/card/card.tsx
+++ my-app/src/components/card/card.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { PlantData } from '../../types/types';
 
-export default class PlantCard extends React.Component<PlantData> {
-  render() {
-    const { id, imgSrc, imgAlt, title, petFriendly, price, date } = this.props;
+export default function PlantCard(props: PlantData) {
+  const { id, imgSrc, imgAlt, title, petFriendly, price, date } = props;
 
-    return (
-      <div className="card" data-id={id}>
-        <img className="card__img" src={imgSrc} alt={imgAlt} />
-        <h3 className="card__title">{title.toUpperCase()}</h3>
-        <div className="card__desc">
-          <p className="desc-price">Price: ${price}</p>
-          <p>delivery: {date}</p>
-          <p>🐱🐶: {petFriendly ? 'safe' : 'dangerous! ❌'}</p>
-        </div>
+  return (
+    <div className="card" data-id={id}>
+      <img className="card__img" src={imgSrc} alt={imgAlt} />
+      <h3 className="card__title">{title.toUpperCase()}</h3>
+      <div className="card__desc">
+        <p className="desc-price">Price: ${price}</p>
+        <p>delivery: {date}</p>
+        <p>🐱🐶: {petFriendly ? 'safe' : 'dangerous! ❌'}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
