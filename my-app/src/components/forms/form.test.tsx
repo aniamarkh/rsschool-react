b391bca656bc3e4a91c45baef663653fa08a8565
aniamarkh <aniamarkh@gmail.com>
2023-04-02 17:54:48 +0300

    fix: form tests

--- my-app/src/components/forms/form.test.tsx
+++ my-app/src/components/forms/form.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { describe, test, expect } from 'vitest';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Form from './form';
@@ -30,36 +30,39 @@ describe('Form', () => {
     expect(submitButton).toBeDefined();
   });
 
-  test('filling the form', async () => {
-    const wrapper = render(<Form />);
-    test('inputs are filled', async () => {
-      const nameInput = wrapper.getByLabelText('Name') as HTMLInputElement;
-      const dateInput = wrapper.getByLabelText('Delivery Date') as HTMLInputElement;
-      const priceSelect = wrapper.getByLabelText('Price') as HTMLSelectElement;
-      const petFriendlyRadio = wrapper.getByLabelText(/Yes/i);
-      const imageInput = wrapper.getByLabelText('Upload an image') as HTMLInputElement;
-      const checkboxInput = wrapper.getByLabelText(
-        'I agree to sell you this plant'
-      ) as HTMLInputElement;
+  test('inputs are filled', async () => {
+    render(<Form />);
+    const nameInput = screen.getByLabelText('Name') as HTMLInputElement;
+    const dateInput = screen.getByLabelText('Delivery Date') as HTMLInputElement;
+    const priceSelect = screen.getByLabelText('Price') as HTMLSelectElement;
+    const petFriendlyRadio = screen.getByLabelText(/Yes/i) as HTMLInputElement;
+    const imageInput = screen.getByLabelText('Upload an image') as HTMLInputElement;
+    const checkboxInput = screen.getByLabelText(
+      'I agree to sell you this plant'
+    ) as HTMLInputElement;
 
-      userEvent.type(nameInput, 'Test Plant');
-      userEvent.type(dateInput, '2023-04-10');
-      userEvent.selectOptions(priceSelect, '26');
-      fireEvent.click(petFriendlyRadio);
-      const file = new File(['(⌐□_□)'], 'test-plant.png', { type: 'image/png' });
-      fireEvent.change(imageInput, { target: { files: [file] } });
-      userEvent.click(checkboxInput);
-      expect(nameInput.value).toBe('Test Plant');
-      expect(dateInput.value).toBe('2023-04-10');
-      expect(priceSelect.value).toBe('26');
-      expect(petFriendlyRadio).toBeChecked();
-      expect(imageInput.files).toBeTruthy();
-      if (imageInput.files) {
-        expect(imageInput.files[0]).toStrictEqual(file);
-        expect(imageInput.files.length).toBe(1);
-      }
-      expect(checkboxInput).toBeChecked();
+    fireEvent.change(nameInput, { target: { value: 'Test Plant' } });
+    fireEvent.change(dateInput, { target: { value: '2023-04-10' } });
+    fireEvent.change(priceSelect, { target: { value: '26' } });
+    act(() => {
+      fireEvent.click(petFriendlyRadio, { target: { value: 'true' } });
     });
+    const file = new File(['(⌐□_□)'], 'test-plant.png', { type: 'image/png' });
+    fireEvent.change(imageInput, { target: { files: [file] } });
+    act(() => {
+      fireEvent.click(checkboxInput, { target: { checked: true } });
+    });
+
+    expect(nameInput.value).toBe('Test Plant');
+    expect(dateInput.value).toBe('2023-04-10');
+    expect(priceSelect.value).toBe('26');
+    expect(petFriendlyRadio.value).toBe('true');
+    expect(imageInput.files).toBeTruthy();
+    if (imageInput.files) {
+      expect(imageInput.files[0]).toStrictEqual(file);
+      expect(imageInput.files.length).toBe(1);
+    }
+    expect(checkboxInput).toBeChecked();
   });
 
   test('renders errors', async () => {
@@ -89,37 +92,5 @@ describe('Form', () => {
     fireEvent.submit(form);
 
     expect(await wrapper.findByText('Give us at least one day ;)')).toBeInTheDocument();
-  });
-
-  test('filling the form', async () => {
-    const wrapper = render(<Form />);
-    test('confirmation message is rendered', async () => {
-      const nameInput = wrapper.getByLabelText('Name') as HTMLInputElement;
-      const dateInput = wrapper.getByLabelText('Delivery Date') as HTMLInputElement;
-      const priceSelect = wrapper.getByLabelText('Price') as HTMLSelectElement;
-      const petFriendlyRadio = wrapper.getByLabelText(/Yes/i);
-      const imageInput = wrapper.getByLabelText('Upload an image') as HTMLInputElement;
-      const checkboxInput = wrapper.getByLabelText(
-        'I agree to sell you this plant'
-      ) as HTMLInputElement;
-
-      userEvent.type(nameInput, 'Test Plant');
-      userEvent.type(dateInput, '2023-04-10');
-      userEvent.selectOptions(priceSelect, '26');
-      fireEvent.click(petFriendlyRadio);
-      const file = new File(['(⌐□_□)'], 'test-plant.png', { type: 'image/png' });
-      fireEvent.change(imageInput, { target: { files: [file] } });
-      userEvent.click(checkboxInput);
-
-      const form = await wrapper.findByRole('form');
-      fireEvent.submit(form);
-
-      expect(await wrapper.findByText('Your plant has been added!')).toBeInTheDocument();
-      expect(await wrapper.findByText('Test plant')).toBeInTheDocument();
-      expect(await wrapper.findByText('delivery: 10.04.2023')).toBeInTheDocument();
-      expect(await wrapper.findByText('$26')).toBeInTheDocument();
-      expect(await wrapper.findByText('🐱🐶: safe')).toBeInTheDocument();
-      expect(plantsData.length).toBe(1);
-    });
   });
 });
