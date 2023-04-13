f1d2c3f22d2d2d684fa38e3049ef5bedce22af5f
aniamarkh <aniamarkh@gmail.com>
2023-04-13 15:17:49 +0300

    feat: add searchState to HomePage component

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -3,10 +3,12 @@ import SearchBar from '../components/searchBar/searchBar';
 import MoviesList from '../components/moviesList/moviesList';
 import { TmdbMovieResult } from '../types/types';
 import { fetchSearchData, fetchPopular } from '../api/api';
+import { RootState } from 'store/store';
+import { useSelector } from 'react-redux';
 
 export default function HomePage() {
+  const searchState = useSelector((state: RootState) => state.searchValue);
   const [movies, setMovies] = useState<TmdbMovieResult[] | null>([]);
-  const [search] = useState<string>(localStorage.getItem('search') || '');
   const [error, setError] = useState<string>('');
   const [loaded, setLoaded] = useState<boolean>(false);
 
@@ -29,11 +31,11 @@ export default function HomePage() {
   );
 
   useEffect(() => {
-    fetchData(search);
-  }, [search, fetchData]);
+    fetchData(searchState.searchValue);
+  }, [searchState.searchValue, fetchData]);
 
-  const handleSearch = (value: string) => {
-    fetchData(value);
+  const handleSearch = (searchValue: string) => {
+    fetchData(searchValue);
   };
 
   return (
